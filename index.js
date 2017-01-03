f06899da351a0f629a185c9b0261764fd8c59797
Helric122915 <helric122915@yahoo.com>
2017-01-03 21:18:25 +0000

    Additions to webserver

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// use "sudo fuser -k PORTNUM/tcp" to release the port if improperly exited
+
 var app = require('express')();
 var http = require('http').Server(app);
 var bodyParser = require('body-parser');
@@ -6,12 +8,17 @@ var port = 3000
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-function fanData(Room_Temp, Fan_Speed){
-  this.Room_Temp = Room_Temp;
-  this.Fan_Speed = Fan_Speed;
+function fanData(Op_Mode, Direction, Manual_Fan_Speed){
+  this.Op_Mode = Op_Mode
+  this.Direction = Direction;
+  this.Manual_Fan_Speed = Manual_Fan_Speed;
 }
 
-var data = new fanData("73", "3600");
+var data = new fanData("No_Mode", "Clockwise", "33");
+
+//app.get('/', function(request, response) {
+//  response.sendFile(__dirname + '/test.html');
+//})
 
 app.get('/CurrentFanData', function(request, response) {
   response.writeHead(200, {"Content-Type": "application/json"});
@@ -24,12 +31,19 @@ app.get('/CurrentFanData', function(request, response) {
 })
 
 app.post('/UpdateFanData', function(request, response) {
-  var Room_Temp = request.body.Room_Temp;
-  var Fan_Speed = request.body.Fan_Speed;
+  var Op_Mode = request.body.Op_Mode
+  var Direction = request.body.Direction;
+  var Manual_Fan_Speed = request.body.Manual_Fan_Speed;
   
-  data = new fanData(Room_Temp, Fan_Speed);
-  
-  response.send('Success');
+  if (typeof Op_Mode !== 'undefined' && typeof Direction !== 'undefined' && typeof Manual_Fan_Speed !== 'undefined')
+  {
+    data = new fanData(Op_Mode, Direction, Manual_Fan_Speed);
+    response.send('Success');
+  }
+  else
+  {
+    response.send('Error, data could not be parsed properly');
+  }
 });
 
 app.listen(port, function(err) {
