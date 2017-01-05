ba7a7cfaeaf6ae3f0efc797d4383d108c61302fe
Helric122915 <helric122915@yahoo.com>
2017-01-05 19:41:21 +0000

    Changed the Get / Post requests to access the database for each mode.

--- index.js
+++ index.js
@@ -17,8 +17,6 @@ var database = mysql.createConnection({
 
 database.connect();
 
-opMode = "Manual"
-
 function ManualData(Manual_Direction, Manual_Fan_Speed){
   this.Manual_Direction = Manual_Direction;
   this.Manual_Fan_Speed = Manual_Fan_Speed;
@@ -32,8 +30,6 @@ function OneTempData(One_Temp_Direction, One_Temp_Low_Speed, One_Temp_Low_Temp, 
   this.One_Temp_High_Temp = One_Temp_High_Temp
 }
 
-var oneTempData = new OneTempData("Clockwise","27","65","45","78");
-
 function TwoTempData(Two_Temp_Low_Speed, Two_Temp_Low_Temp, Two_Temp_High_Speed, Two_Temp_High_Temp){
   this.Two_Temp_Low_Speed = Two_Temp_Low_Speed
   this.Two_Temp_Low_Temp = Two_Temp_Low_Temp
@@ -41,21 +37,46 @@ function TwoTempData(Two_Temp_Low_Speed, Two_Temp_Low_Temp, Two_Temp_High_Speed,
   this.Two_Temp_High_Temp = Two_Temp_High_Temp
 }
 
-var twoTempData = new TwoTempData("27","65","45","78");
-
 //app.get('/', function(request, response) {
 //  response.sendFile(__dirname + '/test.html');
 //})
 
 app.get('/GetOp', function(request, response) {
   response.writeHead(200, {"Content-Type": "application/json"});
+  
+  database.query('SELECT mode FROM OpData WHERE entry_id=1 LIMIT 1', function(err,rows,fields){
+    opMode = rows[0]['mode'];
 
-  var json = JSON.stringify({
-    data:opMode
-  });
+    var json = JSON.stringify({
+      data:opMode
+    });
 
-  response.end(json);
+    response.end(json);
+  })
 })
+
+app.post('/PostOp', function(request,response) {
+  var opMode = request.body.data;
+
+  if (typeof opMode !== 'undefined')
+  {
+    var sql = "REPLACE INTO OpData (entry_id, mode) VALUES(1,?)";
+    var inserts = [opMode];
+
+    sql = mysql.format(sql, inserts);
+    database.query(sql, function(err,rows,fields)
+    {
+      if(!err)
+        response.send('Success');
+      else
+        response.send('Database error');
+    });
+  }
+  else
+  {
+    response.send('Error, data could not be parsed properly');
+  }
+}
 
 app.get('/GetManual', function(request, response) {
   response.writeHead(200, {"Content-Type": "application/json"});
@@ -81,11 +102,13 @@ app.post('/PostManual', function(request,response) {
      var inserts = [Manual_Direction,Manual_Fan_Speed];
 
      sql = mysql.format(sql,inserts);
-     database.query(sql, function(err,rows,fields){});
-
-     manualData = new ManualData(Manual_Direction, Manual_Fan_Speed);
-     opMode = "Manual";
-     response.send('Success');
+     database.query(sql, function(err,rows,fields)
+     {
+       if(!err)
+         response.send('Success');
+       else
+         response.send('Database error');
+     });
   }
   else
   {
@@ -113,12 +136,16 @@ app.post('/PostSchedule', function(request, response) {
 
 app.get('/GetOneTemp', function(request, response) {
   response.writeHead(200, {"Content-Type": "application/json"});
+  
+  database.query('SELECT direction,lowSpeed,lowTemp,highSpeed,highTemp FROM OneTempData WHERE entry_id=1 LIMIT 1', function(err,rows,fields){
+    var oneTempData = new OneTempData(rows[0]['direction'],rows[0]['lowSpeed'],rows[0]['lowTemp'],rows[0]['highSpeed'],rows[0]['highTemp'];
+    
+    var json = JSON.stringify({
+      data:oneTempData
+    });
 
-  var json = JSON.stringify({
-    data:oneTempData
-  });
-
-  response.end(json);
+    response.end(json);
+  })
 })
 
 app.post('/PostOneTemp', function(request, response) {
@@ -128,10 +155,19 @@ app.post('/PostOneTemp', function(request, response) {
   var One_Temp_High_Speed = request.body.One_Temp_High_Speed;
   var One_Temp_High_Temp = request.body.One_Temp_High_Temp;
 
-  if (typeof One_Temp_Direction !== 'undefined' && typeof One_Temp_Low_Speed !== 'undefined' && typeof One_Temp_Low_Temp !== 'undefined' && typeof One_Temp_High_Speed !== 'undefined' && typeof One_Temp_High_Temp !== 'undefined'){
-    oneTempData = new OneTempData(One_Temp_Direction,One_Temp_Low_Speed,One_Temp_Low_Temp,One_Temp_High_Speed,One_Temp_High_Temp);
-    opMode = "OneTemp";
-    response.send('Success');
+  if (typeof One_Temp_Direction !== 'undefined' && typeof One_Temp_Low_Speed !== 'undefined' && typeof One_Temp_Low_Temp !== 'undefined' && typeof One_Temp_High_Speed !== 'undefined' && typeof One_Temp_High_Temp !== 'undefined')
+  {
+    var sql = "REPLACE INTO OneTempData (entry_id,direction,lowSpeed,lowTemp,highSpeed,highTemp) VALUES(1,?,?,?,?,?)";  
+    var inserts = [One_Temp_Direction,One_Temp_Low_Speed,One_Temp_Low_Temp,One_Temp_High_Speed,One_Temp_High_Temp];
+    
+    sql = mysql.format(sql,inserts);
+    database.query(sql, function(err,rows,fields)
+    {
+      if(!err)
+        response.send('Success');
+      else
+        response.send('Database Error');
+    });
   }
   else
   {
@@ -142,11 +178,15 @@ app.post('/PostOneTemp', function(request, response) {
 app.get('/GetTwoTemp', function(request, response) {
   response.writeHead(200, {"Content-Type": "application/json"});
 
-  var json = JSON.stringify({
-    data:twoTempData
-  });
+  database.query('SELECT lowSpeed,lowTemp,highSpeed,highTemp FROM TwoTempData WHERE entry_id=1 LIMIT 1', function(err,rows,fields){
+    var twoTempData = new TwoTempData(rows[0]['lowSpeed'],rows[0]['lowTemp'],rows[0]['highSpeed'],rows[0]['highTemp'];
+    
+    var json = JSON.stringify({
+      data:twoTempData
+    });
 
-  response.end(json);
+    response.end(json)
+  })
 })
 
 app.post('/PostTwoTemp', function(request, response) {
@@ -155,10 +195,19 @@ app.post('/PostTwoTemp', function(request, response) {
   var Two_Temp_High_Speed = request.body.Two_Temp_High_Speed;
   var Two_Temp_High_Temp = request.body.Two_Temp_High_Temp;
 
-  if (typeof Two_Temp_Low_Speed !== 'undefined' && typeof Two_Temp_Low_Temp !== 'undefined' && typeof Two_Temp_High_Speed !== 'undefined' && typeof Two_Temp_High_Temp !== 'undefined'){
-    twoTempData = new TwoTempData(Two_Temp_Low_Speed,Two_Temp_Low_Temp,Two_Temp_High_Speed,Two_Temp_High_Temp);
-    opMode = "TwoTemp";
-    response.send('Success');
+  if (typeof Two_Temp_Low_Speed !== 'undefined' && typeof Two_Temp_Low_Temp !== 'undefined' && typeof Two_Temp_High_Speed !== 'undefined' && typeof Two_Temp_High_Temp !== 'undefined')
+  {
+    var sql = "REPLACE INTO TwoTempData (entry_id,lowSpeed,lowTemp,highSpeed,highTemp) VALUES(1,?,?,?,?)";
+    var inserts = [Two_Temp_Low_Speed,Two_Temp_Low_Temp,Two_Temp_High_Speed,Two_Temp_High_Temp];
+
+    sql = mysql.format(sql,inserts);
+    database.query(sql, function(err,rows,fields)
+    {
+      if(!err)
+        response.send('Success');
+      else
+        response.send('Database error');
+    });
   }
   else
   {
