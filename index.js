8f9a553e60ec4457e7349614fd4e4c02056ffb00
Helric122915 <jbb47@zips.uakron.edu>
2017-04-26 10:26:24 -0400

    Cleaning Webserver up.

--- index.js
+++ index.js
@@ -45,18 +45,18 @@ function OpMode(Mode,Power,RPM){
 
 var power = "false";
 
-function LocationData(Address, Latitude, Longitude){
+function LocationData(Address, Latitude, Longitude) {
     this.Address = Address;
     this.Latitude = Latitude;
     this.Longitude = Longitude;
 }
 
-function ManualData(Manual_Direction, Manual_Fan_Speed){
+function ManualData(Manual_Direction, Manual_Fan_Speed) {
     this.Manual_Direction = Manual_Direction;
     this.Manual_Fan_Speed = Manual_Fan_Speed;
 }
 
-function ScheduleData(Schedule_ID, Begin_Time, End_Time, Direction, Fan_Speed, Day, Enabled){
+function ScheduleData(Schedule_ID, Begin_Time, End_Time, Direction, Fan_Speed, Day, Enabled) {
     this.Schedule_ID = Schedule_ID;
     this.Begin_Time = Begin_Time;
     this.End_Time = End_Time;
@@ -66,14 +66,14 @@ function ScheduleData(Schedule_ID, Begin_Time, End_Time, Direction, Fan_Speed, D
     this.Enabled = Enabled;
 }
 
-function CurrentScheduleData(Begin_Time, End_Time, Direction, Fan_Speed){
+function CurrentScheduleData(Begin_Time, End_Time, Direction, Fan_Speed) {
     this.Begin_Time = Begin_Time;
     this.End_Time = End_Time;
     this.Direction = Direction;
     this.Fan_Speed = Fan_Speed;
 }
 
-function OneTempData(One_Temp_Direction, One_Temp_Low_Speed, One_Temp_Low_Temp, One_Temp_High_Speed, One_Temp_High_Temp){
+function OneTempData(One_Temp_Direction, One_Temp_Low_Speed, One_Temp_Low_Temp, One_Temp_High_Speed, One_Temp_High_Temp) {
     this.One_Temp_Direction = One_Temp_Direction
     this.One_Temp_Low_Speed = One_Temp_Low_Speed
     this.One_Temp_Low_Temp = One_Temp_Low_Temp
@@ -81,7 +81,7 @@ function OneTempData(One_Temp_Direction, One_Temp_Low_Speed, One_Temp_Low_Temp, 
     this.One_Temp_High_Temp = One_Temp_High_Temp
 }
 
-function TwoTempData(Two_Temp_Low_Speed, Two_Temp_Low_Temp, Two_Temp_High_Speed, Two_Temp_High_Temp){
+function TwoTempData(Two_Temp_Low_Speed, Two_Temp_Low_Temp, Two_Temp_High_Speed, Two_Temp_High_Temp) {
     this.Two_Temp_Low_Speed = Two_Temp_Low_Speed
     this.Two_Temp_Low_Temp = Two_Temp_Low_Temp
     this.Two_Temp_High_Speed = Two_Temp_High_Speed
@@ -132,7 +132,7 @@ app.post('/PostTemp', function(request, response) {
 app.get('/GetOp', function(request, response) {
     response.writeHead(200, {"Content-Type": "application/json"});
     
-    database.query('SELECT mode, rpm FROM OpData WHERE entry_id=1 LIMIT 1', function(err,rows,fields){
+    database.query('SELECT mode, rpm FROM OpData WHERE entry_id=1 LIMIT 1', function(err,rows,fields) {
 	if (typeof rows !== 'undefined' && rows.length > 0) {
 	    var opMode = new OpMode(rows[0]['mode'],power,rows[0]['rpm']);
 
@@ -350,7 +350,7 @@ app.get('/GetCurrentSchedule', function(request, response) {
     })
 })
 
-app.post('/CreateSchedule', function(request, response){
+app.post('/CreateSchedule', function(request, response) {
     var Schedule_ID = request.body.Schedule_ID;
     var Begin_Time = request.body.Begin_Time;
     var End_Time = request.body.End_Time;
@@ -385,7 +385,7 @@ app.post('/CreateSchedule', function(request, response){
     response.send(res);
 })
 
-app.post('/UpdateSchedule', function(request, response){
+app.post('/UpdateSchedule', function(request, response) {
     var Schedule_ID = request.body.Schedule_ID;
     var Begin_Time = request.body.Begin_Time;
     var End_Time = request.body.End_Time;
@@ -441,7 +441,7 @@ app.post('/UpdateSchedule', function(request, response){
     response.send(res);
 })
 
-app.post('/ToggleSchedule', function(request, response){
+app.post('/ToggleSchedule', function(request, response) {
     var Schedule_ID = request.body.Schedule_ID;
     var Toggle = request.body.Toggle;
 
@@ -465,7 +465,7 @@ app.post('/ToggleSchedule', function(request, response){
 	response.send('Error, data could not be parsed properly');
 })
 
-app.post('/DeleteSchedule', function(request, response){
+app.post('/DeleteSchedule', function(request, response) {
     var Schedule_ID = request.body.Schedule_ID;
 
     if (typeof Schedule_ID !== 'undefined') {
@@ -528,7 +528,7 @@ app.post('/PostOneTemp', function(request, response) {
 app.get('/GetTwoTemp', function(request, response) {
     response.writeHead(200, {"Content-Type": "application/json"});
 
-    database.query('SELECT lowSpeed,lowTemp,highSpeed,highTemp FROM TwoTempData WHERE entry_id=1 LIMIT 1', function(err,rows,fields){
+    database.query('SELECT lowSpeed,lowTemp,highSpeed,highTemp FROM TwoTempData WHERE entry_id=1 LIMIT 1', function(err,rows,fields) {
 	if (typeof rows !== 'undefined' && rows.length > 0) {
 	    var twoTempData = new TwoTempData(rows[0]['lowSpeed'],rows[0]['lowTemp'],rows[0]['highSpeed'],rows[0]['highTemp']);
 	    
